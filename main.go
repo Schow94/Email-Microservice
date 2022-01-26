43529678d280ece697d0631bea80010b3befcb9f
Schow94 <Schow94@gmail.com>
2022-01-26 08:04:05 -0800

    Heroku deployment

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -56,6 +57,15 @@ func main(){
 
 		Email(data)
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+	
+
+	r.Run(fmt.Sprintf(":%s", port))
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 }
